f6815c265a425e06ff337e2c4cc01fa798a78c28
Peter Wang <pwang@australiacloud.com.au>
2022-11-08 13:40:14 +1000

    init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let count = 0;
+  $: doubled = count * 2;
+
+  function incrementCount() {
+    count += 1;
+  }
+</script>
+
+<button on:click={incrementCount}>
+  Clicked {count}
+  {count === 1 ? "time" : "times"}
+</button>
+
+<p>{count} doubled is {doubled}</p>
